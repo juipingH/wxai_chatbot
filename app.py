fd58092581e5dea456b59688b05fdf9024ddbe18
juipingH <141833061+juipingH@users.noreply.github.com>
2024-11-01 18:25:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ task_options = {
 # Model Options
 model_options = {
     "mistralai/mistral-large": "Mistral Large",
-    "meta-llama/llama-3-1-70b-instruct": "Llama 3-1 70B Instruct",
+    "meta-llama/llama-3-70b-instruct": "Llama 3-1 70B Instruct",
     "meta-llama/llama-3-1-8b-instruct": "LLAMA 3-1 8B INSTRUCT",
     "ibm/granite-3-8b-instruct": "Granite 3 8B Instruct",
     "ibm/granite-3-2b-instruct": "Granite 3 2B Instruct",
